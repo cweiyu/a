12d2c07925b8029e1c43443a403965a9a2ad8bb7
cweiyu <36878863+cweiyu@users.noreply.github.com>
2019-05-13 20:40:05 +0800

    Add files via upload

---
+++ (2).py
@@ -0,0 +1,8 @@
+﻿num = eval(input())
+alist = list()
+for x in range(1,num+1):
+    if x%3 != 0 and x%5 != 0:
+        alist.append(x)
+    if x%3 == 0 and x%6 == 0:
+        alist.append(x)
+print(len(alist))
